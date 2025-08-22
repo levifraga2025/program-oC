36d45ebc88fcfff48460a5b062aea8480d8871ee
levifraga2025 <jrtagima@gmail.com>
2025-08-22 01:56:02 +0000

    Aula incremento decremento

---
+++ n2_nc3_2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    Atribuuição Simples (=)
+    Atribuição com Soma (+=)
+    Atribuição com Subtração (-=)
+    Atribuição com Multiplicação (*=)
+    Atribuiçao com Divisão (/=)
+    */
+   int numero1 = 10, numnero2, resultado;
+
+   resultado = 10;
+   printf("Resultado: %d\n", resultado);
+
+   //ressultado = resultado + 20;
+   resultado += 20;
+   printf("Resultado: %d\n", resultado);
+
+   //resultado = resultado - numero1;
+   resultado -= numero1;
+   printf("Resultado: %d\n", resultado);
+   
+   
+   //resultado = resultado * 5;
+   resultado *= 5;
+   printf("Resultado: %d\n", resultado);
+
+   //resultado = resutlado / 2;
+   resultado /= 2;
+   printf("Rsultado: %d\n", resultado);
+
+
+
+
+
+
+
+}
