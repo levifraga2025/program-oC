c4551b45a5ceca9323717fe1d15f6ddac6e376ab
levifraga2025 <jrtagima@gmail.com>
2026-02-14 20:57:47 +0000

    Cadastro dos territórios para o jogo War

--- Desafio_WAR_Novato/War_Estruturado_Novato.C
+++ Desafio_WAR_Novato/War_Estruturado_Novato.C
@@ -7,3 +7,28 @@ struct territorio {
     char cor[10];
     int tropas;
 };
+
+int main(){
+    //Definindo um array de structs para armazenar 5 territórios
+    struct Territorios territorios[5];
+    int i;
+
+    printf("--- Cadastro de Territorios ---\n");
+
+    //Cadastro dos territórios
+    for(int i =0; i <5; i++) {
+        printf("\nTerritorios %d:\n", i +1);
+
+        printf("Nome: ");
+        scanf("%[^\n]s", territorios[i].nome); // Lê o nome do território
+        getchar(); // Limpa o buffer do teclado
+
+        printf("Cor do exército: ");
+        scanf("%[^\n]s", territorios[i].cor); // Lê a cor do exército
+        getchar(); // Limpa o buffer do teclado
+
+        printf("Quantidade de tropas: ");
+        scanf("%d", &territorios[i].tropas); // Lê o número de tropas
+        getchar(); // Limpa o buffer do teclado
+    }
+}
