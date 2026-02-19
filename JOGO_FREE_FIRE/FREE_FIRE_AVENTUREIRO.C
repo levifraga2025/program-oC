6cb4d4b4c72d80f4822eaca817a52ee20a538ff4
levifraga2025 <jrtagima@gmail.com>
2026-02-19 16:00:00 +0000

    adicionadno Protótipos, Variáveis de controle, e funções de vetor e lista

--- JOGO_FREE_FIRE/FREE_FIRE_AVENTUREIRO.C
+++ JOGO_FREE_FIRE/FREE_FIRE_AVENTUREIRO.C
@@ -17,3 +17,25 @@ typedef struct No {
     Item dados;
     struct No* proximo;
 } No;
+
+// VARIÁVEIS DE CONTROLE
+Item mochilaVetor[MAX_ITENS];
+int totalVetor = 0;
+No* cabecaLista = NULL;
+int contadorComparacoes = 0;
+
+// PROTÓTIPOS
+void menuPrincipal();
+void limparBuffer();
+const char* GetRaridade(int r);
+
+// FUNÇÕES DE VETOR
+void inserirVetor();
+void listarVetor();
+void buscarBinariaVetor();
+void ordenarVetorNome();
+
+// FUNÇÕES DE LISTA
+void inserirLista();
+void listarLista();
+void removerLista();
