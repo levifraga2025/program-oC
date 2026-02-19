a49cad0c9f7dd91fb68e217f4d7fa649cd31bd2b
levifraga2025 <jrtagima@gmail.com>
2026-02-19 16:08:27 +0000

    corrigindo o código

--- JOGO_FREE_FIRE/FREE_FIRE_AVENTUREIRO.C
+++ JOGO_FREE_FIRE/FREE_FIRE_AVENTUREIRO.C
@@ -7,35 +7,12 @@
 
 // 1. ESTRUTURAS TEMÁTICAS (FREE FIRE)
 typedef struct {
-    char nome[30];      // Ex: AK47, Colete Nv3, Inalador
-    char tipo[20];      // Ex: AR, SMG, Equipamento, Consumivel
+    char nome[30];      
+    char tipo[20];      
     int quantidade;
-    int raridade;       // 1: Comum, 2: Raro, 3: Lendario
 } Item;
 
 typedef struct No {
     Item dados;
     struct No* proximo;
 } No;
-
-// VARIÁVEIS DE CONTROLE
-Item mochilaVetor[MAX_ITENS];
-int totalVetor = 0;
-No* cabecaLista = NULL;
-int contadorComparacoes = 0;
-
-// PROTÓTIPOS
-void menuPrincipal();
-void limparBuffer();
-const char* GetRaridade(int r);
-
-// FUNÇÕES DE VETOR
-void inserirVetor();
-void listarVetor();
-void buscarBinariaVetor();
-void ordenarVetorNome();
-
-// FUNÇÕES DE LISTA
-void inserirLista();
-void listarLista();
-void removerLista();
