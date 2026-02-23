f531fc185b8b0bfd19fc5815bd16d7811735ca64
levifraga2025 <jrtagima@gmail.com>
2026-02-23 19:12:44 +0000

    inserindo as FILAS

--- JOGO_TETRIS_STACK_NOVATO/TETRIS_STACK_NOVATO.C
+++ JOGO_TETRIS_STACK_NOVATO/TETRIS_STACK_NOVATO.C
@@ -32,3 +32,12 @@ void inicializarFila(FilaPecas *f) {
     f->tras = -1;
     f->total_elementos = 0;
 }
+// Verifica se a fila está cheia
+int filaCheia(FilaPecas *f) {
+    return f->total_elementos == TAMANHO_FILA;
+}
+
+// Verifica se a fila está vazia
+int filaVazia(FilaPecas *f) {
+    return f->total_elementos == 0;
+}
