5fbb874ffeab690a25f252e931f547fcc8aa1928
levifraga2025 <jrtagima@gmail.com>
2026-02-23 19:13:25 +0000

    iniciando com enqueue e dequeue

--- JOGO_TETRIS_STACK_NOVATO/TETRIS_STACK_NOVATO.C
+++ JOGO_TETRIS_STACK_NOVATO/TETRIS_STACK_NOVATO.C
@@ -41,3 +41,23 @@ int filaCheia(FilaPecas *f) {
 int filaVazia(FilaPecas *f) {
     return f->total_elementos == 0;
 }
+// Insere uma nova peça no final da fila (Enqueue)
+void inserirPeca(FilaPecas *f) {
+    if (filaCheia(f)) {
+        printf("\n[AVISO] A fila esta cheia! Nao e possivel adicionar novas pecas.\n");
+        return;
+    }
+    // Lógica circular: o próximo índice volta ao zero se atingir o limite
+    f->tras = (f->tras + 1) % TAMANHO_FILA;
+    f->itens[f->tras] = gerarPeca();
+    f->total_elementos++;
+}
+
+// Remove a peça da frente da fila para "jogar" (Dequeue)
+void jogarPeca(FilaPecas *f) {
+    if (filaVazia(f)) {
+        printf("\n[AVISO] Nao ha pecas na fila para jogar!\n");
+        return;
+    }
+    Peca removida = f->itens[f->frente];
+    printf("\n>>> Voce jogou a peca [%c %d]!\n", removida.nome, removida.id);
