4b5d648e668211d34dae2d65857fa1161078c59f
levifraga2025 <jrtagima@gmail.com>
2026-02-23 19:11:47 +0000

    gerando variáveis

--- JOGO_TETRIS_STACK_NOVATO/TETRIS_STACK_NOVATO.C
+++ JOGO_TETRIS_STACK_NOVATO/TETRIS_STACK_NOVATO.C
@@ -9,3 +9,26 @@ typedef struct {
     char nome;
     int id;
 } Peca;
+
+typedef struct {
+    Peca itens[TAMANHO_FILA];
+    int frente;
+    int tras;
+    int total_elementos;
+} FilaPecas;
+
+int contador_id = 0;
+
+Peca gerarPeca() {
+    char tipos[] = {'I', 'O', 'T', 'L'};
+    Peca nova;
+    nova.nome = tipos[rand() % 4];
+    nova.id = contador_id++;
+    return nova;
+}
+
+void inicializarFila(FilaPecas *f) {
+    f->frente = 0;
+    f->tras = -1;
+    f->total_elementos = 0;
+}
