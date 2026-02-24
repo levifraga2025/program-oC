99078a24aa33de53165a91a014341c0cecd51fd8
levifraga2025 <jrtagima@gmail.com>
2026-02-24 18:32:49 +0000

    iniciando o código com as bibliotecas, structs

---
+++ JOGO_TETRIS_STACK_MESTRE/TETRIS_STACK_MESTRE.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAMANHO_FILA 5
+#define TAMANHO_PILHA 3
+
+typedef struct {
+    char nome;
+    int id;
+} Peca;
+
+typedef struct {
+    Peca itens[TAMANHO_FILA];
+    int frente;
+    int tras;
+    int total_elementos;
+} FilaPecas;
+
+typedef struct {
+    Peca itens[TAMANHO_PILHA];
+    int topo;
+} PilhaReserva;
