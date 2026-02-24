add596cb35da05b0021fc7552771d830f865e625
levifraga2025 <jrtagima@gmail.com>
2026-02-24 00:50:25 +0000

    adicionando PUSH E POP

--- JOGO_TETRIS_STACK_AVENTUREIRO/TETRIS_STACK_AVENTUREIRO.C
+++ JOGO_TETRIS_STACK_AVENTUREIRO/TETRIS_STACK_AVENTUREIRO.C
@@ -29,6 +29,7 @@ void inicializarFila(FilaPecas *f) {
     f->tras = -1;
     f->total_elementos = 0;
 }
+
 void inicializarPilha(PilhaReserva *p) {
     p->topo = -1;
 }
@@ -45,6 +46,7 @@ void inserirFila(FilaPecas *f) {
         f->total_elementos++;
     }
 }
+
 // --- LOGICA DO JOGO ---
 void jogarPeca(FilaPecas *f) {
     if (f->total_elementos == 0) {
@@ -60,6 +62,7 @@ void jogarPeca(FilaPecas *f) {
     // Repõe automaticamente para manter a fila cheia
     inserirFila(f);
 }
+
 // Reservar Peça (Move da Fila para a Pilha - PUSH)
 void reservarPeca(FilaPecas *f, PilhaReserva *p) {
     if (f->total_elementos == 0) {
@@ -84,4 +87,31 @@ void reservarPeca(FilaPecas *f, PilhaReserva *p) {
     
     // Repõe a fila
     inserirFila(f);
+}
+
+// Usar Reserva (Move da Pilha para o Jogo - POP)
+void usarReserva(PilhaReserva *p) {
+    if (p->topo == -1) {
+        printf("\nNao ha pecas na reserva!\n");
+        return;
+    }
+    Peca pecaUsada = p->itens[p->topo];
+    p->topo--;
+    printf("\n[USO RESERVA] Voce usou a peca reservada: [%c %d]\n", pecaUsada.nome, pecaUsada.id);
+}
+
+// --- INTERFACE ---
+
+void exibirEstado(FilaPecas *f, PilhaReserva *p) {
+    printf("\n======= STATUS DO JOGO =======\n");
+    printf("FILA (Proximas): ");
+    for (int i = 0; i < f->total_elementos; i++) {
+        int idx = (f->frente + i) % TAMANHO_FILA;
+        printf("[%c %d] ", f->itens[idx].nome, f->itens[idx].id);
+    }
+    
+    printf("\nRESERVA (Topo): ");
+    if (p->topo == -1) printf("[ VAZIA ]");
+    else printf("[%c %d]", p->itens[p->topo].nome, p->itens[p->topo].id);
+    printf("\n------------------------------\n");
 }
