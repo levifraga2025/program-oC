ca66bf4e9395f6cd5deadf8501ef273e4fc88074
levifraga2025 <jrtagima@gmail.com>
2026-03-02 23:45:43 +0000

    Definição da estrutura da Sala

--- Desafio_Detective_Quest/Detective_Quest.C
+++ Desafio_Detective_Quest/Detective_Quest.C
@@ -2,3 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Definição da estrutura da Sala (Nó da Árvore Binária)
+typedef struct Sala {
+    char nome[50];
+    struct Sala *esquerda;
+    struct Sala *direita;
+} Sala;
